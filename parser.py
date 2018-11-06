e0d2820875b71bfa6c767fe177b6c2fd2ca32b82
seraphalo <hehezeno@gmail.com>
2018-11-05 20:43:03 -0800

    finish from beginning up until logical_or_expr

--- parser.py
+++ parser.py
@@ -34,8 +34,43 @@ def p_statement(p):
                  | while_statement
                  | jump_statement'''
 
+def p_expr_statement(p):
+    '''expr_statement : SEMI
+                      | assignment_expr SEMI'''
+
+def p_selection_statement(p):
+    '''selection_statement : IF LPAREN expr RPAREN statement
+                           | IF LPAREN expr RPAREN statement ELSE statement'''
+
+def p_for_statement(p):
+    '''for_statement : FOR LPAREN ID IN expr RPAREN statement'''
+
+def p_do_while_statement(p):
+    '''do_while_statement : DO statement WHILE LPAREN expr RPAREN SEMI'''
+
+def p_while_statement(p):
+    '''while_statement : WHILE LPAREN expr RPAREN statement'''
+
+def p_jump_statement(p):
+    '''while_statement : NEXT SEMI
+                       | BREAK SEMI
+                       | RETURN SEMI
+                       | RETURN expr SEMI'''
+def p_expr(p):
+    '''expr : conditional_expr'''
+
+def p_assignment_expr(p):
+    '''assignment_expr : conditional_expr
+                       | conditional_expr EQUALS conditional_expr'''
+
+def p_logical_or_expr(p):
+    '''logical_or_expr : logical_or_expr
+                       | logical_or_expr or_optional
+       or_optional : or_optional OR logical_or_expr
+                   | OR logical_or_expr'''
+
 def p_eof(p):
-    '''EOF : '''
+    '''EOF :'''
     pass
 
 
