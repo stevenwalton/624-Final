9ce52e00ea3d04eb4a25c3bf399962f1823856d4
Steven Walton <walton.stevenj@gmail.com>
2018-11-06 20:08:35 -0800

    Formatting

--- parser.py
+++ parser.py
@@ -14,11 +14,11 @@ lexer = lex.lex()
 def p_interpreter_block(p):
     '''
     interpreter_block : EOF
-                         | block_multiple EOF
+                      | block_multiple EOF
        block_multiple : block_multiple statement
-                     | block_multiple function_decl
-                     | statement
-                     | function_decl
+                      | block_multiple function_decl
+                      | statement
+                      | function_decl
     '''
 
 
@@ -33,24 +33,24 @@ def p_compound_statement(p):
 def p_statement(p):
     '''
     statement : compound_statement
-                 | expr_statement
-                 | selection_statement
-                 | for_statement
-                 | do_while_statement
-                 | while_statement
-                 | jump_statement
+              | expr_statement
+              | selection_statement
+              | for_statement
+              | do_while_statement
+              | while_statement
+              | jump_statement
     '''
 
 def p_expr_statement(p):
     '''
     expr_statement : SEMI
-                      | assignment_expr SEMI
+                   | assignment_expr SEMI
     '''
 
 def p_selection_statement(p):
     '''
     selection_statement : IF LPAREN expr RPAREN compound_statement
-                           | IF LPAREN expr RPAREN compound_statement ELSE compound_statement
+                        | IF LPAREN expr RPAREN compound_statement ELSE compound_statement
     '''
 
 def p_for_statement(p):
@@ -71,9 +71,9 @@ def p_while_statement(p):
 def p_jump_statement(p):
     '''
     jump_statement : NEXT SEMI
-                       | BREAK SEMI
-                       | RETURN SEMI
-                       | RETURN expr SEMI
+                   | BREAK SEMI
+                   | RETURN SEMI
+                   | RETURN expr SEMI
     '''
 def p_expr(p):
     '''
@@ -83,19 +83,19 @@ def p_expr(p):
 def p_assignment_expr(p):
     '''
     assignment_expr : conditional_expr
-                       | conditional_expr EQUALS conditional_expr
+                    | conditional_expr EQUALS conditional_expr
     '''
 
 def p_conditional_expr(p):
     '''
     conditional_expr : logical_or_expr
-                        | logical_or_expr TERNARY conditional_expr ELSE conditional_expr
+                     | logical_or_expr TERNARY conditional_expr ELSE conditional_expr
     '''
 
 def p_logical_or_expr(p):
     '''
     logical_or_expr : logical_and_expr
-                       | logical_and_expr or_multiple
+                    | logical_and_expr or_multiple
        or_multiple : or_multiple OR logical_and_expr
                    | OR logical_and_expr
     '''
@@ -103,7 +103,7 @@ def p_logical_or_expr(p):
 def p_logical_and_expr(p):
     '''
     logical_and_expr : equality_expr
-                        | equality_expr and_multiple
+                     | equality_expr and_multiple
        and_multiple : and_multiple AND equality_expr
                     | AND equality_expr
     '''
@@ -111,14 +111,14 @@ def p_logical_and_expr(p):
 def p_equality_expr(p):
     '''
     equality_expr : relational_expr
-                     | relational_expr NE equality_expr
-                     | relational_expr EQ equality_expr
+                  | relational_expr NE equality_expr
+                  | relational_expr EQ equality_expr
     '''
 
 def p_relational_expr(p):
     '''
     relational_expr : add_expr
-                       | add_expr relational_multiple
+                    | add_expr relational_multiple
        relational_multiple : relational_multiple LT add_expr
                            | relational_expr LE add_expr
                            | relational_expr GT add_expr
@@ -132,7 +132,7 @@ def p_relational_expr(p):
 def p_add_expr(p):
     '''
     add_expr : mult_expr
-                | mult_expr add_multiple
+             | mult_expr add_multiple
        add_multiple : add_multiple PLUS mult_expr
                     | add_multiple MINUS mult_expr
                     | PLUS mult_expr
@@ -142,69 +142,69 @@ def p_add_expr(p):
 def p_mult_expr(p):
     '''
     mult_expr : seq_expr
-                 | seq_expr TIMES mult_expr
-                 | seq_expr DIVIDE mult_expr
-                 | seq_expr MODULO mult_expr
+              | seq_expr TIMES mult_expr
+              | seq_expr DIVIDE mult_expr
+              | seq_expr MODULO mult_expr
     '''
 
 def p_seq_expr(p):
     '''
     seq_expr : exp_expr
-                | exp_expr COLON exp_expr
+             | exp_expr COLON exp_expr
     '''
 
 def p_exp_expr(p):
     '''
     exp_expr : unary_expr
-                | unary_expr XOR exp_expr
+             | unary_expr XOR exp_expr
     '''
 
 def p_unary_expr(p):
     '''
     unary_expr : postfix_expr
-                  | NOT unary_expr
-                  | PLUS unary_expr
-                  | MINUS unary_expr
+               | NOT unary_expr
+               | PLUS unary_expr
+               | MINUS unary_expr
     '''
 
 def p_postfix_expr(p):
     '''
     postfix_expr : primary_expr
-                    | primary_expr LBRACKET RBRACKET
-                    | primary_expr LBRACKET expr RBRACKET
-                    | primary_expr LBRACKET COMMA RBRACKET
-                    | primary_expr LBRACKET COMMA expr RBRACKET
-                    | primary_expr LBRACKET expr COMMA expr RBRACKET
-                    | primary_expr LPAREN RPAREN
-                    | primary_expr LPAREN argument_expr_list RPAREN
-                    | primary_expr PERIOD ID
+                 | primary_expr LBRACKET RBRACKET
+                 | primary_expr LBRACKET expr RBRACKET
+                 | primary_expr LBRACKET COMMA RBRACKET
+                 | primary_expr LBRACKET COMMA expr RBRACKET
+                 | primary_expr LBRACKET expr COMMA expr RBRACKET
+                 | primary_expr LPAREN RPAREN
+                 | primary_expr LPAREN argument_expr_list RPAREN
+                 | primary_expr PERIOD ID
     '''
 
 def p_primary_expr(p):
     '''
     primary_expr : ID
-                    | constant
-                    | LPAREN expr RPAREN
+                 | constant
+                 | LPAREN expr RPAREN
     '''
 
 def p_argument_expr_list(p):
     '''
     argument_expr_list : argument_expr
-                          | argument_expr COMMA argument_expr_list
+                       | argument_expr COMMA argument_expr_list
     '''
 
 def p_argument_expr(p):
     '''
     argument_expr : conditional_expr
-                     | ID EQUALS conditional_expr
+                  | ID EQUALS conditional_expr
     '''
 
 def p_constant(p):
     '''
     constant : INTEGER
-                | FLOAT
-                | STRING
-                | CHARACTER
+             | FLOAT
+             | STRING
+             | CHARACTER
     '''
 
 def p_function_decl(p):
@@ -220,18 +220,18 @@ def p_return_type_spec(p):
 def p_type_spec(p):
     '''
     type_spec : type_spec_options
-                 | type_spec_options DOLLAR
+              | type_spec_options DOLLAR
        type_spec_options : TYPEVOID
-        | TYPENULL
-        | TYPELOGICAL
-        | TYPEINTEGER
-        | TYPEFLOAT
-        | TYPESTRING
-        | TYPEOBJECT
-        | TYPEOBJECT object_class_spec
-        | TYPENUMERIC
-        | PLUS
-        | TIMES
+                         | TYPENULL
+                         | TYPELOGICAL
+                         | TYPEINTEGER
+                         | TYPEFLOAT
+                         | TYPESTRING
+                         | TYPEOBJECT
+                         | TYPEOBJECT object_class_spec
+                         | TYPENUMERIC
+                         | PLUS
+                         | TIMES
     '''
 
 def p_object_class_spec(p):
@@ -242,19 +242,19 @@ def p_object_class_spec(p):
 def p_param_list(p):
     '''
     param_list : LPAREN param_option RPAREN
-        param_option : TYPEVOID
-        | param_spec
-        | param_spec param_spec_multiple
-        param_spec_multiple : COMMA param_spec
-        | param_spec_multiple COMMA param_spec
+    param_option : TYPEVOID
+                 | param_spec
+                 | param_spec param_spec_multiple
+    param_spec_multiple : COMMA param_spec
+                        | param_spec_multiple COMMA param_spec
     '''
 
 def p_param_spec(p):
     '''
     param_spec : type_spec ID
-        | LBRACKET type_spec ID EQUALS value_option RBRACKET
-        value_option : constant
-        | ID
+               | LBRACKET type_spec ID EQUALS value_option RBRACKET
+    value_option : constant
+                 | ID
     '''
 
 def p_eof(p):
