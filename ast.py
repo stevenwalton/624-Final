6bc1bd49f547331b160c9ba881eaecabca179a9a
seraphalo <hehezeno@gmail.com>
2018-11-26 21:36:20 -0800

    adding constant

--- ast.py
+++ ast.py
@@ -144,3 +144,20 @@ class NodeVisitor(object):
         """
         for c in node:
             self.visit(c)
+
+class Constant(Node):
+    __slots__ = ('type', 'value', 'coord', '__weakref__')
+    def __init__(self, type, value, coord=None):
+        self.type = type
+        self.value = value
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    def __iter__(self):
+        return
+        yield
+
+    attr_names = ('type', 'value', )
