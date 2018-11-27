22b3a1a1369d0ea699a122bb687c0803bab701e0
seraphalo <hehezeno@gmail.com>
2018-11-26 22:36:40 -0800

    adding ID

--- ast.py
+++ ast.py
@@ -320,3 +320,19 @@ class Assignment(Node):
             yield self.rvalue
 
     attr_names = ('op', )
+
+class ID(Node):
+    __slots__ = ('name', 'coord', '__weakref__')
+    def __init__(self, name, coord=None):
+        self.name = name
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    def __iter__(self):
+        return
+        yield
+
+    attr_names = ('name', )
