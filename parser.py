5841565714a376a799253383bcca5ab124b61df1
Steven Walton <swalton2@uoregon.edu>
2018-12-04 16:26:50 -0800

    Interpreter should work better.
    Functions are still broken though. trying to solve how to run functions
    in the interpreter. Might be too much at this point because a node is
    returning and I need to just get the straight functions.
    Most functionality should be back to normal though.

--- parser.py
+++ parser.py
@@ -852,7 +852,10 @@ def tree():
     # prog = 'x = 0; z = 0; for (i in 1 : 5) {y = x; if (i == 3) x = 0; else x = x + 1; z = z + 1;}'
     # prog = 'x = 3; if (x%2 != 0) x = x + 1;'
     #prog = '3==4 | 4==4;'
-    prog = 'x = 0; y = 0; function (int) foo (int x, int y, int z) { return x + y + z;} function (int) bar (int x, int y) { if (x != y) return x * y; else return x;} x = bar(5, 5); y = bar(2,3);'
+    #prog = 'x = 0; y = 0; function (int) foo (int x, int y, int z) { return x + y + z;} function (int) bar (int x, int y) { if (x != y) return x * y; else return x;} x = bar(5, 5); y = bar(2,3);'
+    #prog = 'function(int) foo(int x) {return x+1;}'
+    #prog = 'function(int) foo(int x) {return x+1;} foo(1);'
+    prog = 'function(int) foo(int x, int y) {x=x+1;y=x+1;z=x+y; return z;}'
 
     # prog = 'x = 0; y = 0; if (x != y) x = x + 1; else y = y + 1; x = 5;'
     # prog = 'x = 0; for (i in 1 : 5) {y = x + i; x = x + 1;}'
